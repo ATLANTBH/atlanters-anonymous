e5cfc03570dac66f84cd77ab102cdeecfbae2c9d
fvedad <vedad.fejzagic@atlantbh.com>
2019-03-25 19:29:28 +0100

    Remove space

--- app.js
+++ app.js
@@ -14,28 +14,28 @@ app.use(cookieParser(process.env.COOKIE_S));
 sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
 
 	if (eraseDatabaseOnSync) {
-			//createUsersWithMessages();
+		//createUsersWithMessages();
 	}
 	
 	app.use('/auth', auth);
 
 	app.use(function(err, req, res, next) {
-			res.status(err.status || 500);
-			let error = req.app.get('env') === 'development' ? err : {}
-			console.log(error);
-			res.json({
-					message: err.message,
-					error: {}
-			})
+		res.status(err.status || 500);
+		let error = req.app.get('env') === 'development' ? err : {}
+		console.log(error);
+		res.json({
+				message: err.message,
+				error: {}
+		})
 	})
 
 	app.get('/user', (req, res) => {
-			models.User.findAll()
-					.then(user => {
-							console.log(user)
-							res.sendStatus(200);
-					})
-					.catch(err => console.log(err))
+		models.User.findAll()
+				.then(user => {
+						console.log(user)
+						res.sendStatus(200);
+				})
+				.catch(err => console.log(err))
 	});
 
 	app.listen(process.env.PORT, () => {
