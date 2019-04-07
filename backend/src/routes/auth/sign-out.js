8545d00629846973f01fd9bd639efa0f305b8894
fvedad <vedad.fejzagic@atlantbh.com>
2019-04-07 18:09:13 +0200

    Implement sign out

---
+++ backend/src/routes/auth/sign-out.js
@@ -0,0 +1,11 @@
+export default async (req, res, next) => {
+  const user = req.user;
+  const token = req.token;
+  try {
+    await user.removeAuthenticationToken(token);
+    res.status(200).send();
+  }
+  catch(e) {
+    next(new Error(e));
+  }
+}
