84a3dc39640f61ca146ba406bea4cce5d31a52c4
fvedad <vedad.fejzagic@atlantbh.com>
2019-04-07 16:54:44 +0200

    Add auth token for users, refactor code

--- backend/src/routes/auth/index.js
+++ backend/src/routes/auth/index.js
@@ -3,11 +3,16 @@ import signUp from './sign-up';
 import signIn from './sign-in';
 import signOut from './sign-out';
 
+function verifyToken(req, res, next) {
+  const header = req.headers['authorization'];
+  next();
+}
+
 export default app => {
   const router = Router();
 
   router.post('/sign-up', signUp(app));
-  router.post('/sign-in', signIn(app));
+  router.post('/sign-in', verifyToken, signIn(app));
 
   return router;
 };
--- backend/src/routes/auth/sign-in.js
+++ backend/src/routes/auth/sign-in.js
@@ -1,24 +1,32 @@
 import { hash, compare } from 'bcrypt';
 
+async function authenticateUser(email, password, User, next) {
+  const user = await User.findByEmail(email);
+  if (!user) next(new Error('User with this email does not exist'));
+  else {
+    const isPasswordEqual = await compare(password, user.password);
+    if (!isPasswordEqual) next(new Error('Password incorrect'));
+    return user;
+  }
+}
+
 export default ({ models }) => {
   const { User } = models;
   return async (req, res, next) => {
     const { email, password } = req.body;
-    const user = await User.findByEmail(email);
-    if (!user) next(new Error('User with this email does not exist'));
-    else {
-      const isPasswordEqual = await compare(password, user.password);
-      if (!isPasswordEqual) next(new Error('Password incorrect'));
-      else {
-        res.cookie('user_id', user.id, {
-          httpOnly: true,
-          secured: true,
-          signed: true,
-        });
-        res.json({
-          msg: 'Logged in',
-        });
-      }
+    const user = await authenticateUser(email, password, User, next);
+    try {
+      const token = await user.generateAuthenticationToken();
+      res.cookie('user_id', user.id, {
+        httpOnly: true,
+        secured: true,
+        signed: true,
+      });
+      res.header('x-auth', token).send(user);
+      return token;
+    }
+    catch (error) {
+      next(new Error(error))
     }
   };
 };
