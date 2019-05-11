35e74a53ca44ee79b0dff5f44971fcd5725f5ecc
fvedad <vedad.fejzagic@atlantbh.com>
2019-05-11 11:09:45 +0200

    Fix case sensitive

--- backend/src/routes/middlewares/verify-token.js
+++ backend/src/routes/middlewares/verify-token.js
@@ -1,7 +1,7 @@
 export default ({ models }) => {
   const { User } = models;
   return async (req, res, next) => {
-    const token = req.header('x-auth');
+    const token = req.header('X-Auth');
     const user = await User.findByAuthenticationToken(token);
     if (!user) next(new Error('Please log in to continue'));
     req.user = user;
