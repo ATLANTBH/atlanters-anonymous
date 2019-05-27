c6c62a9b69b289e837f9745c35567e55ef305ca0
fvedad <vedad.fejzagic@atlantbh.com>
2019-05-27 20:33:00 +0200

    Add tests to integration

---
+++ backend/tests/mocks/data/answer.js
@@ -0,0 +1,16 @@
+export default {
+  newAnswer: [
+    {
+      "Your favorite food?": "fasdfsaf"
+    },
+    {
+      "Your favorite movie genre":  ["Sci-fi", "Biography"]
+    },
+    {
+      "Additional input": ""
+    },
+    {
+      "How much are you satisfied?": 2
+    }
+  ]
+} 
