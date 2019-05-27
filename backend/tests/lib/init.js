03c8e437f4c5aee0452d24361029fc5c7c407c0e
fvedad <vedad.fejzagic@atlantbh.com>
2019-05-27 22:52:26 +0200

    Delete commented code, add mode check

--- backend/tests/lib/init.js
+++ backend/tests/lib/init.js
@@ -6,10 +6,3 @@ export default async function getModels() {
   const sequelize = await initConnection(process.env.MODE);
   return await init.models(sequelize, eraseDatabaseOnSync);
 }
-
-// export default async function getModels() {
-  //   const eraseDatabaseOnSync = true;
-  //   const sequelize = await initConnection(eraseDatabaseOnSync);
-  //   const models = await initModels(sequelize);
-  //   return models;
-  // }
