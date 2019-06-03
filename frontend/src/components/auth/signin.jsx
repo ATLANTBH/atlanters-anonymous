286f95a08b2046fbbc8966656a688df67daf96b4
fvedad <vedad.fejzagic@atlantbh.com>
2019-06-03 21:36:57 +0200

    Add navbar

---
+++ frontend/src/components/auth/signin.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Sign in</h1>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
