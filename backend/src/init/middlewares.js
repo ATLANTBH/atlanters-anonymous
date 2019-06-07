6ffa09b5000da14db983a01b7ae6bcd80c81b032
fvedad <vedad.fejzagic@atlantbh.com>
2019-06-07 19:10:32 +0200

    Add cors

--- backend/src/init/middlewares.js
+++ backend/src/init/middlewares.js
@@ -1,7 +1,14 @@
 import { json } from 'body-parser';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 export default async ({ expressApp }) => {
   expressApp.use(json());
   expressApp.use(cookieParser(process.env.COOKIE_S));
+  expressApp.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
 };
