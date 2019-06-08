fcc3a4e4b72f2b6f40d77cc28cd97d9731005384
fvedad <vedad.fejzagic@atlantbh.com>
2019-06-08 10:39:14 +0200

    Change cors options

--- backend/src/init/middlewares.js
+++ backend/src/init/middlewares.js
@@ -2,13 +2,17 @@ import { json } from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+const corsOptions = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  preflightContinue: true,
+  maxAge: 600,
+};
+
 export default async ({ expressApp }) => {
   expressApp.use(json());
   expressApp.use(cookieParser(process.env.COOKIE_S));
-  expressApp.use(
-    cors({
-      origin: 'http://localhost:3000',
-      credentials: true,
-    })
-  );
+  expressApp.options('*', cors(corsOptions));
+  expressApp.use(cors(corsOptions));
 };
