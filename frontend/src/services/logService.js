75c2ca8eefd7add44dd5e1ff39779c81f843f446
fvedad <vedad.fejzagic@atlantbh.com>
2019-06-12 23:10:01 +0200

    Add dashboard inf scroll

--- frontend/src/services/logService.js
+++ frontend/src/services/logService.js
@@ -1,13 +1,14 @@
 import Raven from "raven-js";
 function init() {
-  Raven.config("https://03511474ec8f481ea5522f6b9703959d@sentry.io/1477514", {
-    release: "1-0-0",
-    environment: "dev"
-  }).install();
+  // Raven.config("https://03511474ec8f481ea5522f6b9703959d@sentry.io/1477514", {
+  //   release: "1-0-0",
+  //   environment: "dev"
+  // }).install();
 }
 
 function log(error) {
-  Raven.captureException(error);
+  console.log(error);
+  //Raven.captureException(error);
 }
 
 export default {
