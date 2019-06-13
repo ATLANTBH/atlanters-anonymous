cd6afb5b80f3ca79101b3683eeeb319ba216f776
fvedad <vedad.fejzagic@atlantbh.com>
2019-06-13 21:41:00 +0200

    Add get poll templates by draft with count

--- backend/src/routes/poll-template/poll-templates.draft.get.js
+++ backend/src/routes/poll-template/poll-templates.draft.get.js
@@ -1,9 +1,9 @@
 export default ({ models }) => {
   const { PollTemplate, Poll } = models;
   return async (req, res, next) => {
-    const isDraft = req.params.isDraft;
+    const { count, isDraft } = req.params;
     try {
-      const pollTemplates = await PollTemplate.findAllByDraft(isDraft);
+      const pollTemplates = await PollTemplate.findAllByDraft(count, isDraft);
       res.send(pollTemplates);
     } catch (error) {
       next(new Error(error));
