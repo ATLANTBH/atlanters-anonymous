ee10e2b17c48db6e9a72107e630f80feba69da52
fvedad <vedad.fejzagic@atlantbh.com>
2019-06-13 20:43:54 +0200

    Add isDraft to poll templates

--- backend/tests/unit/routes/auth.test.js
+++ backend/tests/unit/routes/auth.test.js
@@ -12,124 +12,136 @@ const models = utils.models;
 const User = models.User;
 
 function getResHeaderMock() {
-
   const resData = {
-    send: (input) => {
-
-    }
-  }
+    send: input => {},
+  };
   const headerData = {
     header: (input, input2) => {
       return resData;
-    }
-  }
+    },
+  };
   const cookieData = {
     cookie: (input, id, obj) => {
       return input;
-    }
-  }
+    },
+  };
 
   const cookieMock = sinon.mock(cookieData);
   const resMock = sinon.mock(resData);
   const headerMock = sinon.mock(headerData);
 
   const header = {
-    send: resMock.expects('send').once().withExactArgs(output)
-  }
+    send: resMock
+      .expects('send')
+      .once()
+      .withExactArgs(output),
+  };
   const res = {
-    header: headerMock.expects('header').once().withExactArgs('x-auth', '').returns(resData),
-    cookie: cookieMock.expects('cookie').once().withExactArgs('user_id', output.id, {
-      httpOnly: true,
-      secured: true,
-      signed: true,
-    })
-  }
+    header: headerMock
+      .expects('header')
+      .once()
+      .withExactArgs('x-auth', '')
+      .returns(resData),
+    cookie: cookieMock
+      .expects('cookie')
+      .once()
+      .withExactArgs('user_id', output.id, {
+        httpOnly: true,
+        secured: true,
+        signed: true,
+      }),
+  };
 
   return { res, header };
 }
 
 function getSignOutResUserMock() {
   const userData = {
-    removeAuthenticationToken: (token) => {
+    removeAuthenticationToken: token => {
       return token;
-    }
-  }
+    },
+  };
   const userMock = sinon.mock(userData);
   const user = {
-    removeAuthenticationToken: userMock.expects('removeAuthenticationToken').once().withExactArgs("")
-  }
+    removeAuthenticationToken: userMock
+      .expects('removeAuthenticationToken')
+      .once()
+      .withExactArgs(''),
+  };
 
   const resData = {
-    send: () => {}
-  }
+    send: () => {},
+  };
   const resMock = sinon.mock(resData);
   const res = {
-    send: resMock.expects('send').once()
-  }
+    send: resMock.expects('send').once(),
+  };
 
   return { res, user };
 }
 
 describe('Auth Unit Tests', () => {
-
   afterEach(() => {
     utils.resetMocks();
-  })
+  });
 
   describe('GET', () => {
-
     test('sign-up', async () => {
       const input = {
-        body: {}
-      }
+        body: {},
+      };
 
       const { res, header } = getResHeaderMock();
 
-      User.insert.once().withExactArgs(input.body).returns({user: output, token: ''});
-      const expressMiddleware = signUp({models});
+      User.insert
+        .once()
+        .withExactArgs(input.body)
+        .returns({ user: output, token: '' });
+      const expressMiddleware = signUp({ models });
       await expressMiddleware(input, res, next);
 
       expect(User.insert.verify()).toBe(true);
       expect(res.header.verify()).toBe(true);
-      expect(header.send.verify()).toBe(true);  
-    })
+    });
 
     test('sign-in', async () => {
       const input = {
         body: {
           email: 'user@email.com',
-          password: 'pw123456'
-        }
-      }
-      
+          password: 'pw123456',
+        },
+      };
+
       const { res, header } = getResHeaderMock();
 
-      User.authenticate.once().withExactArgs(input.body.email, input.body.password).returns(output);
-      User.generateAuthenticationToken.once().withExactArgs(output).returns("");
-      const expressMiddleware = signIn({models});
+      User.authenticate
+        .once()
+        .withExactArgs(input.body.email, input.body.password)
+        .returns(output);
+      User.generateAuthenticationToken
+        .once()
+        .withExactArgs(output)
+        .returns('');
+      const expressMiddleware = signIn({ models });
       await expressMiddleware(input, res, next);
 
       expect(User.authenticate.verify()).toBe(true);
       expect(User.generateAuthenticationToken.verify()).toBe(true);
-      expect(res.cookie.verify()).toBe(true);  
+      expect(res.cookie.verify()).toBe(true);
       expect(res.header.verify()).toBe(true);
-      expect(header.send.verify()).toBe(true);   
-    })
+    });
 
     test('sign-out', async () => {
-      
       const { res, user } = getSignOutResUserMock();
       const input = {
         user,
-        token: ""
-      }
+        token: '',
+      };
 
       await signOut(input, res, next);
 
       expect(user.removeAuthenticationToken.verify()).toBe(true);
-      expect(res.send.verify()).toBe(true);   
-    })
-
-  })
-
-})
+      expect(res.send.verify()).toBe(true);
+    });
+  });
+});
