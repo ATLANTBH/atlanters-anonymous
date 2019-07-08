c26357e9b64bbf589b34a2bfbaf14e8ee171bf4b
fvedad <vedad.fejzagic@atlantbh.com>
2019-07-08 21:04:45 +0200

    Add design changes

--- frontend/src/components/feedback/feedback-send.jsx
+++ frontend/src/components/feedback/feedback-send.jsx
@@ -4,6 +4,9 @@ import { sendFeedback } from "../../services/feedbackService";
 import checkmark from "../../assets/images/feedback/checkmark.png";
 import xmark from "../../assets/images/feedback/xmark.png";
 import Utils from "../../utils";
+import classNames from 'classnames';
+import { MoonLoader } from 'react-spinners';
+import Loader from 'react-loader-spinner'
 
 const { feedback } = Utils.string.PATHS;
 
@@ -34,16 +37,13 @@ class FeedbackSend extends Form {
     const { isBeingSent, error } = this.state;
     return (
       <div className="feedback-container send">
-        <div className="form feedback-card">
-          {isBeingSent ? (
-            <div className="result-container">
-              <div className="title">Please wait...</div>
-              <hr />
-              <div className="text">
-                Sending your feedback. Please do not close this window.
-              </div>
-            </div>
-          ) : (
+        {isBeingSent ? <div className="moon-loader-wrapper"><Loader
+          type="Oval"
+          color="#00a3da"
+          height="60"
+          width="60"
+        />
+        </div> : <div className={classNames("form feedback-card feedback-send", { 'error': error })}>
             <div className="result-container">
               <div className="header">
                 <img
@@ -64,8 +64,8 @@ class FeedbackSend extends Form {
                   : "Thank you! Your feedback is greatly appreciated."}
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        }
       </div>
     );
   }
