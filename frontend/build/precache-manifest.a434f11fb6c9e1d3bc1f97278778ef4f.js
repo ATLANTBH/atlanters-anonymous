6bcb58f629c8a098398a7cd9e1360ae89b9622f9
fvedad <vedad.fejzagic@atlantbh.com>
2019-07-10 20:32:59 +0200

    Edit build

---
+++ frontend/build/precache-manifest.a434f11fb6c9e1d3bc1f97278778ef4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601f507cdf70ae1c0f60f15829ce1b37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "602923d4e4646b356828",
+    "url": "/static/css/2.019f27f6.chunk.css"
+  },
+  {
+    "revision": "74635c904f008460c782",
+    "url": "/static/css/main.ad6c32ca.chunk.css"
+  },
+  {
+    "revision": "602923d4e4646b356828",
+    "url": "/static/js/2.d6a13490.chunk.js"
+  },
+  {
+    "revision": "74635c904f008460c782",
+    "url": "/static/js/main.4ce1344d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5e1d162e634a102d7da8db597825a764",
+    "url": "/static/media/SourceSansPro-Regular.5e1d162e.otf"
+  },
+  {
+    "revision": "71096f93ab59ad7c66a963700c422f54",
+    "url": "/static/media/overpass-regular.71096f93.otf"
+  }
+]);
