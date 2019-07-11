5e02d8d581c868c225f12219936607ce68c73a27
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2019-07-11 13:07:49 +0200

    Small change

--- frontend/src/components/feedback/feedback-confirm.jsx
+++ frontend/src/components/feedback/feedback-confirm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Form from "../common/form";
 import Utils from "../../utils";
+import Form from "../common/form";
 
 const { feedback, feedbackSend } = Utils.string.PATHS;
 
