8278ae7c5953127ac1ebf84da7ad52c6303a1553
fvedad <vedad.fejzagic@atlantbh.com>
2019-08-16 02:06:47 +0200

    Add mail sending for chat messages

--- backend/src/sockets/events/chat.js
+++ backend/src/sockets/events/chat.js
@@ -8,6 +8,8 @@ export default (io, models, socket) => {
       const feedback = await models.Feedback.findById(feedbackId);
       if (!feedback) throw new Error('Feedback does not exist');
       if (feedback.isClosed) throw new Error('This case is closed');
+      if (userId === -1)
+        models.Feedback.sendMail({ text: feedbackId + ': ' + text });
       const message = await postMessage(models, feedbackId, userId, text);
       io.sockets.emit(feedbackId, message);
     } catch (err) {
