c3dae60d36a9fd64a7d5b06392a4dafe1ea44b2a
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2019-09-12 14:45:16 +0200

    Fix validation error

--- backend/src/init/models.js
+++ backend/src/init/models.js
@@ -9,21 +9,11 @@ export default async (sequelize, eraseDatabaseOnSync) => {
   } catch (error) {
     console.log(error);
   }
-  await createFirstUser(models);
   if (eraseDatabaseOnSync) {
     await createUsersWithMessages(models);
   }
 
   return models;
-};
-
-const createFirstUser = async models => {
-  await models.User.create({
-    name: 'Admin',
-    surname: 'Admin',
-    email: 'atlantersanonymous@atlantbh.com',
-    password: '$2b$10$z186VQxBAHCxS9Ry6yJLJORMMHSgKfglSYz1wq/tgw0hw/tp4I7Zu',
-  });
 };
 
 const createUsersWithMessages = async models => {
