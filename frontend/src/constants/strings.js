99e238ef18b39515a335d2bc8313c05f0018547b
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2019-10-24 13:30:39 +0200

    Change frontend maximum num characters

--- frontend/src/constants/strings.js
+++ frontend/src/constants/strings.js
@@ -3,3 +3,5 @@ export const SEEN_EVENT = "seen";
 
 export const USER_LAST_SEEN = "userLastSeenAt";
 export const ANONYMOUS_LAST_SEEN = "anonymLastSeenAt";
+
+export const MESSAGE_LENGTH = 30000;
--- frontend/src/utils/strings.js
+++ frontend/src/utils/strings.js
@@ -1,10 +1,12 @@
+import { MESSAGE_LENGTH } from "../constants/strings";
+
 export const removeSpaces = string => {
   return string.replace(/ /g, "");
 };
 
 export const validateInputMessage = message => {
-  if (message.length > 1000)
-    return "Messages greater than 1000 characters are not allowed";
+  if (message.length > MESSAGE_LENGTH)
+    return `Messages greater than ${MESSAGE_LENGTH} characters are not allowed`;
   return null;
 };
 
