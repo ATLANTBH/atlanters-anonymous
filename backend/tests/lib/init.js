87c8b6d448ce304ebc1457ece8c29642e62ea394
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2020-06-11 19:56:04 +0200

    change erase to false in tests

--- backend/tests/lib/init.js
+++ backend/tests/lib/init.js
@@ -2,7 +2,7 @@ import init from '../../src/init';
 import initConnection from './db';
 
 export default async function getModels() {
-  const eraseDatabaseOnSync = true;
+  const eraseDatabaseOnSync = false;
   const sequelize = await initConnection(false);
   return await init.models(sequelize, eraseDatabaseOnSync);
 }
